7b8f8a1a20ad0455466eea8345f65ff8ca8fca73
Avinash Verma <avinashverma@Avinashs-MacBook-Pro.local>
2023-12-15 15:26:00 +0530

    added 2 pages

---
+++ src/pages/about.js
@@ -0,0 +1,6 @@
+import * as React from 'react';
+export default function About(){
+    return (<main>
+        <h1> About Gatsby</h1>
+    </main>);
+}
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+
+
+export default function IndexPages(){
+    return (<main>
+        <h1>
+            Hello Gatsby
+        </h1>
+    </main>)
+}
